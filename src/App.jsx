17b2a01baeedbfc0840c27e6ee66460de4ab9776
Ismail x Pepper <93700134+IsmailxPepper@users.noreply.github.com>
2026-05-26 16:29:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 export default function App() {
   return (
-    <div>
+    <div style={{
+      minHeight: '100vh',
+      background: '#1a001f',
+      color: 'white',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '40px',
+      fontWeight: 'bold'
+    }}>
       Shaima ❤️
     </div>
   )
